1a54f0382ddf4f220e3ffa7ba1eb86be93921f83
Kasey Arnold <kaseyarnold15@gmail.com>
2021-02-03 19:41:31 +0000

    Automatically backed up by Learn

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -6,7 +6,7 @@ class Pet extends React.Component {
       <div className="card">
         <div className="content">
           <a className="header">
-            {/*'♀' OR '♂' */}
+            
             PET NAME
           </a>
           <div className="meta">
